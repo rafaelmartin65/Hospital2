40bc9c33913797f554098137475ea44d81636b8a
rafaelmartin65 <rmartin65@gmail.com>
2023-09-25 17:41:55 +0100

    add try catch to Principal

--- Hospital2/src/com/cga/sanidad/Principal.java
+++ Hospital2/src/com/cga/sanidad/Principal.java
@@ -2,6 +2,9 @@ package com.cga.sanidad;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jca.cci.CannotGetCciConnectionException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 import com.cga.sanidad.dao.PacienteDao;
 import com.cga.sanidad.pojo.Paciente;
@@ -15,8 +18,23 @@ public class Principal {
 		PacienteDao pacienteDao = (PacienteDao) parametros.getBean("PacienteDao");
 
 		Paciente pac = (Paciente) parametros.getBean("pac");
-
-		System.out.println(pacienteDao.save(pac));
+		
+		//System.out.println(pacienteDao.save(pac));
+		
+		try {
+			
+			pacienteDao.save(pac);
+			
+		}catch (CannotGetJdbcConnectionException e) {
+			System.out.println("Credenciales, Confuiguración!!! ");
+			e.printStackTrace();
+			
+		}catch (DataAccessException ex) {
+			System.out.println("Error en SQL!!!");
+			ex.printStackTrace();
+			
+		
+		}
 
 		System.out.println(pac + "\n");
 		pac.imprimirHistorial();
